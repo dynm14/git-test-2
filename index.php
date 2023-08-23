6c3c63a9517c95a47759d647db19c50575f184b2
Dinar Muhammad Akbar <dinarm.akbar14@gmail.com>
2023-08-23 09:51:36 +0700

    Menambhkan list

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
     <h1>Hello Wolrd!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita nam numquam quo id, eum magni soluta, nulla cum fuga adipisci odio officiis! Repudiandae, illo nisi cum blanditiis incidunt nihil dolores labore temporibus explicabo recusandae fuga qui nostrum? Necessitatibus rerum quaerat atque fugiat iure possimus dolorem repellat ab quos a tenetur, enim nemo accusantium hic earum quod dolores magni veniam? Esse accusantium nemo doloribus dolor aspernatur enim praesentium quidem quasi qui architecto similique atque repellendus beatae earum officiis incidunt veniam pariatur fugiat doloremque ex ipsum, impedit deleniti? Reprehenderit quisquam inventore at fuga expedita quam, soluta itaque id accusantium aliquam quae praesentium repellat corporis aliquid perferendis. Atque voluptate perferendis quis, aliquid rem debitis explicabo iure dolor tempore esse praesentium blanditiis maiores aspernatur veritatis, fuga illum at facilis maxime itaque deserunt. Atque optio voluptates a doloribus maxime dolore distinctio veritatis nemo facere laboriosam pariatur quo, voluptatem assumenda eligendi ullam consequuntur consectetur modi. Maxime natus, consequatur tenetur voluptatum enim voluptas corporis aliquam praesentium eaque alias harum fugiat, architecto facere. Doloribus repellendus voluptate hic nihil est animi? Harum accusamus assumenda debitis dolore, blanditiis pariatur architecto placeat atque deserunt adipisci laudantium. Molestias officiis aliquam repellendus sequi, tenetur voluptatibus saepe in eius, voluptas a doloremque nostrum!</p>
+
+    <ul>
+        <li>Dinar</li>
+        <li>Dinar</li>
+        <li>Dinar</li>
+    </ul>
 </body>
 </html>
